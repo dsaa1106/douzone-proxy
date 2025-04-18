a78a3e20d903a052a8a4c80ea174a5e09027e446
dsaa1106 <103019205+dsaa1106@users.noreply.github.com>
2025-04-18 16:23:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_alarms():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    imfort os
+    import os
     port = int(os.environ.get("PORT", 5000))  # Render가 할당한 포트를 사용
     print(f"==> 포트 확인: {port}")
     app.run(host='0.0.0.0', port=port)
